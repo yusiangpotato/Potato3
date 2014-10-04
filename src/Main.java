a0fdafe59202d978c8a1bd98b3ce7451b42ae703
iPokeU <ianyongasd@gmail.com>
2014-10-04 15:52:54 +0800

    Better hierarchy of gui classes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.List;
-
 import javafx.application.Application;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
@@ -11,16 +8,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Pane root = new Pane();
+        StageSecondaryHelper ssh = new StageSecondaryHelper();
         primaryStage.setTitle("Hello World");
-        root.getChildren().add(new Circle(10,10,100));
 
-
-        Scene sx = new Scene(root, 1000, 650);
+        Scene sx = new Scene(ssh.createSecondaryStage(primaryStage), 1000, 650);
         primaryStage.setScene(sx);
         primaryStage.setResizable(false);
-        MenuBarHelper mbh = new MenuBarHelper();
-        root.getChildren().add(mbh.createMenuBar(primaryStage));
         primaryStage.show();
     }
 
---
+++ src/StageSecondaryHelper.java
@@ -0,0 +1,18 @@
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class StageSecondaryHelper {
+
+	public BorderPane createSecondaryStage(Stage stage) {
+		BorderPane bp = new BorderPane();
+		
+		MenuBarHelper mbh = new MenuBarHelper();
+		bp.setTop(mbh.createMenuBar(stage));
+		//bp.setLeft();
+		//bp.setCenter();
+		//bp.setRight();
+		
+		return bp;
+	}
+
+}

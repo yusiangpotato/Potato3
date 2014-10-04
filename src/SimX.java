ad62c459a85ec9043011baab3fc39f7684241247
yusiangpotato <yusiang1998@gmail.com>
2014-10-04 15:54:37 +0800

    -3

--- src/SimX.java
+++ src/SimX.java
@@ -7,8 +7,8 @@ public class SimX extends Thread implements Runnable{
     ArrayList<Particle> particleList = new ArrayList<Particle>();
 
     public SimX (){
-//
 
+//
     }
     @Override
     public void run(){

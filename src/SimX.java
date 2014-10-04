7eea2cd339b92552a829fcdba7b3cf83c9ba8222
yusiangpotato <yusiang1998@gmail.com>
2014-10-04 15:14:17 +0800

    -1

--- src/SimX.java
+++ src/SimX.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+
 /**
  * Created by Yusiang on 4/10/2014.
  */
 public class SimX extends Thread implements Runnable{
-    //ArrayList<>
+    ArrayList<Particle> particleList = new ArrayList<Particle>();
+
     public SimX (){
 
 

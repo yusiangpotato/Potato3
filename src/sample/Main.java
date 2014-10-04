e2a0e82c1906b63dc588e6a1dc5de4c5b5d97ece
yusiangpotato <yusiang1998@gmail.com>
2014-10-04 14:46:09 +0800

    --

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,14 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Pane root = new Pane();
         primaryStage.setTitle("Hello World");
+        root.getChildren().add(new Circle(10,10,100));
+
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }

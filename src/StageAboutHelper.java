8f55cea53f68cf6edd7227afe677f9ca41786566
iPokeU <ianyongasd@gmail.com>
2014-10-04 20:38:31 +0800

    Labels for About and Instructions

--- src/StageAboutHelper.java
+++ src/StageAboutHelper.java
@@ -1,3 +1,7 @@
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class StageAboutHelper {
@@ -5,7 +9,13 @@ public class StageAboutHelper {
 	public static void createStageAbout() {
 		Stage s = new Stage();
 		s.setTitle("About");
+		BorderPane bp = new BorderPane();
+		s.setScene(new Scene(bp));
 		
+		Text txtAbout = new Text("Temp"); //TODO
+		txtAbout.setTextAlignment(TextAlignment.CENTER);
+		
+		bp.setCenter(txtAbout);
 		s.setResizable(false);
 		s.show();
 	}
--- src/StageInstructionsHelper.java
+++ src/StageInstructionsHelper.java
@@ -1,3 +1,7 @@
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class StageInstructionsHelper {
@@ -5,7 +9,13 @@ public class StageInstructionsHelper {
 	public static void createStageInstructions() {
 		Stage s = new Stage();
 		s.setTitle("Instructions");
+		BorderPane bp = new BorderPane();
+		s.setScene(new Scene(bp));
 		
+		Text txtInstructions = new Text("Temp"); //TODO
+		txtInstructions.setTextAlignment(TextAlignment.CENTER);
+		
+		bp.setCenter(txtInstructions);
 		s.setResizable(false);
 		s.show();
 	}

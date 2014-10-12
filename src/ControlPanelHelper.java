d042fee1f30ae20d3a9e43d41dc2f0bb2e5af823
iPokeU <ianyongasd@gmail.com>
2014-10-12 17:24:34 +0800

    Sliders

--- src/ControlPanelHelper.java
+++ src/ControlPanelHelper.java
@@ -1,12 +1,16 @@
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
 public class ControlPanelHelper {
     SimX simX ;
+    Slider sdrCollision, sdrExplosion;
     public VBox createControlPanel(final SimX simX) {
         VBox vb = new VBox(10); //spacing between elements
         vb.setAlignment(Pos.CENTER);
@@ -17,9 +21,33 @@ public class ControlPanelHelper {
         Button btnTemp1 = new Button("1");
         Button btnTemp2 = new Button("2");
         Button btnTemp3 = new Button("3");
-        Slider sdrCollision = new Slider(0, 1, 0.5);
-        Slider sdrExplosion = new Slider(0, 1, 0.5);
-        vb.getChildren().addAll(btnTemp1, btnTemp2, btnTemp3, sdrCollision, sdrExplosion);
+        sdrCollision = new Slider(0, 1, 0.5);
+        sdrCollision.setShowTickMarks(true);
+        sdrCollision.setShowTickLabels(true);
+        sdrCollision.setMajorTickUnit(0.25f);
+        sdrCollision.setMinorTickCount(4);
+        sdrCollision.setBlockIncrement(0.05f);
+        final Label lblCollision = new Label("Collision probability: " + String.format("%.2f", sdrCollision.getValue()));
+        sdrExplosion = new Slider(0, 1, 0.5);
+        sdrExplosion.setShowTickMarks(true);
+        sdrExplosion.setShowTickLabels(true);
+        sdrExplosion.setMajorTickUnit(0.25f);
+        sdrExplosion.setMinorTickCount(4);
+        sdrExplosion.setBlockIncrement(0.05f);
+        final Label lblExplosion = new Label("Explosion probability: " + String.format("%.2f", sdrExplosion.getValue()));
+        vb.getChildren().addAll(btnTemp1, btnTemp2, btnTemp3, sdrCollision, lblCollision, sdrExplosion, lblExplosion);
+        sdrCollision.valueProperty().addListener(new ChangeListener<Number>(){
+        	public void changed(ObservableValue<? extends Number> observable,
+        			Number oldValue, Number newValue){
+        		lblCollision.setText("Collision probability: " + String.format("%.2f", newValue));
+        	}
+        });
+        sdrExplosion.valueProperty().addListener(new ChangeListener<Number>(){
+        	public void changed(ObservableValue<? extends Number> observable,
+        			Number oldValue, Number newValue){
+        		lblExplosion.setText("Explosion probability: " + String.format("%.2f", newValue));
+        	}
+        });
         btnTemp1.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
@@ -33,6 +61,22 @@ public class ControlPanelHelper {
             }
         });
         return vb;
+    }
+    
+    public void setCollisionChance(float cc){
+    	sdrCollision.setValue(cc);
+    }
+    
+    public float getCollisionChance(){
+    	return (float)sdrCollision.getValue();
+    }
+    
+    public void setExplosionChange(float ec){
+    	sdrExplosion.setValue(ec);
+    }
+    
+    public float getExplosionChange(){
+    	return (float)sdrExplosion.getValue();
     }
 
 }

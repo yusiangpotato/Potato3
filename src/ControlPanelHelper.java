a7cad82f72bc66068625fa4392aac2ecb9403d38
iPokeU <ianyongasd@gmail.com>
2014-10-12 16:55:04 +0800

    Revert "Revert "Push to pull""
    This reverts commit 036350d35963105538ec5c145af44781fff6bbde.

--- src/ControlPanelHelper.java
+++ src/ControlPanelHelper.java
@@ -1,6 +1,7 @@
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
@@ -16,7 +17,9 @@ public class ControlPanelHelper {
         Button btnTemp1 = new Button("1");
         Button btnTemp2 = new Button("2");
         Button btnTemp3 = new Button("3");
-        vb.getChildren().addAll(btnTemp1, btnTemp2, btnTemp3);
+        Slider sdrCollision = new Slider(0, 1, 0.5);
+        Slider sdrExplosion = new Slider(0, 1, 0.5);
+        vb.getChildren().addAll(btnTemp1, btnTemp2, btnTemp3, sdrCollision, sdrExplosion);
         btnTemp1.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent mouseEvent) {
